1bb3214cb8234a63d8ffc17e105818cda3368b64
WiRight <ruslan.mahin1@gmail.com>
2018-06-08 10:50:48 +0300

    save work pre done

---
+++ assets/js/bar.js
@@ -0,0 +1,10 @@
+import '../css/bar.css';
+
+export default function bar() {
+  const a = () => {
+    return {
+      name: 'bar'
+    }
+  };
+  return a()
+}
---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+import bar from './bar'
+import main from './main'
+
+main();
+const a = bar();
+
+console.log('from bar', a);
